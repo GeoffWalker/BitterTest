df15a398b84b8f396c6cbb5ba8efd235da872219
Geoff Walker <34051140+GeoffWalker@users.noreply.github.com>
2019-01-26 00:10:34 -0400

    more name updates

--- includes/connect.php
+++ includes/connect.php
@@ -2,11 +2,11 @@
 //these are defined as constants
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'Po:l00lk-');
 define('DB_NAME', 'bitter-geoffwalker');
 
 global $con;
-	  $con = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+	  $con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
